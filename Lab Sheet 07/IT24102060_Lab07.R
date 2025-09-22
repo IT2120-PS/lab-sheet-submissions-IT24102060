25f9d5802365061e032f731bb2c03d5b6a97d722
IT24102060 <it24102060@my.sliit.lk>
2025-09-22 23:09:08 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102060_Lab07.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\HP.DESKTOP-HMVTQ0R\\Desktop\\IT24102060")
+
+#Exercise
+
+#1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#2
+pexp(2, rate = 1/3)
+
+#3
+
+#i)
+
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+#ii)
+qnorm(0.95, mean = 100, sd = 15)
